efd3fd158a9144f29895b31bdf412c99a3ce666f
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-26 23:54:05 -0300

    Column, Row, Box

--- LayoutEjemplo/app/src/main/java/com/example/layoutejemplo/MainActivity.kt
+++ LayoutEjemplo/app/src/main/java/com/example/layoutejemplo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.layoutejemplo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.layoutejemplo.ui.theme.LayoutEjemploTheme
 
@@ -22,16 +25,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    MyLayoutEjemplo()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyLayoutEjemplo(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Red),
+            contentAlignment = Alignment.Center
+        ) {Text(text = "asdsadsa")}
+            Row(modifier =
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+            )
+            {
+                Box(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Green),
+                    contentAlignment = Alignment.Center
+                ){Text(text = "asdsadsa")}
+                Box(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Blue),
+                    contentAlignment = Alignment.Center
+                ){ Text(text = "asdsadsa")}
+            }
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .background(Color.Magenta),
+            contentAlignment = Alignment.BottomCenter
+        ){ Text(text = "asdsadsa")}
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     LayoutEjemploTheme {
+        MyLayoutEjemplo()
     }
 }
