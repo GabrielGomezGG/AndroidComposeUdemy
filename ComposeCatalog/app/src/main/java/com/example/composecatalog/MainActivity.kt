960628d4856da5d93ed5cb1a838ff136286bab35
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-27 00:31:03 -0300

    Text

---
+++ ComposeCatalog/app/src/main/java/com/example/composecatalog/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.composecatalog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.composecatalog.ui.theme.ComposeCatalogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCatalogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyText()
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Texto normal")
+        Text(text = "Texto negro", fontWeight = FontWeight.Bold)
+        Text(text = "Texto liviana", fontWeight = FontWeight.Light)
+        Text(text = "Texto cursiva", fontFamily = FontFamily.Cursive)
+        Text(text = "Texto h1", fontSize = 20.sp)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCatalogTheme {
+        MyText()
+    }
+}
