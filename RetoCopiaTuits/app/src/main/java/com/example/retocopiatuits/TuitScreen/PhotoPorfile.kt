815de1e2717a0cbb63b9630fb6036aea82425abd
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-29 16:15:09 -0300

    modificaciones a lo creador & agregado del comentario

--- RetoCopiaTuits/app/src/main/java/com/example/retocopiatuits/TuitScreen/PhotoPorfile.kt
+++ RetoCopiaTuits/app/src/main/java/com/example/retocopiatuits/TuitScreen/PhotoPorfile.kt
@@ -1,22 +1,24 @@
 package com.example.retocopiatuits.TuitScreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.retocopiatuits.R
 
 @Composable
-fun MyPhotoProfile(modifier: Modifier){
+fun MyPhotoProfile(modifier: Modifier = Modifier){
 
-    Image(
-        painter = painterResource(id = R.drawable.profile),
-        contentDescription = "photo profile",
-        modifier = modifier
-            .clip(CircleShape)
-    )
+    Box(modifier = modifier.size(60.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "photo profile",
+            Modifier.clip(CircleShape)
+        )
+    }
 }
--- RetoCopiaTuits/app/src/main/java/com/example/retocopiatuits/TuitScreen/TuitScreen.kt
+++ RetoCopiaTuits/app/src/main/java/com/example/retocopiatuits/TuitScreen/TuitScreen.kt
@@ -1,7 +1,11 @@
 package com.example.retocopiatuits.TuitScreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -10,16 +14,20 @@ import androidx.compose.ui.unit.dp
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun MyTuitScreen(){
-    Box(modifier = Modifier
+fun MyTuitScreen() {
+
+    Column(modifier = Modifier
         .fillMaxSize()
-        .background(Color(0xFF181841))
-        .padding(8.dp)
-    ) {
-        Row() {
-            MyPhotoProfile(Modifier.weight(1f))
-            Spacer(modifier = Modifier.size(12.dp))
-            MyAutorTuit(Modifier.weight(5f))
+        .background(color = Color.Black)) {
+        Row(modifier = Modifier
+            .padding(8.dp)) {
+            MyPhotoProfile()
+            Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                MyAutorTuit()
+                Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", color = Color.White)
+            }
         }
     }
+
+
 }
