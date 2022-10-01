26ae80696c3c8ca8f5920e5abfbc44bb943cb611
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-30 23:32:41 -0300

    add BottomNavigation & BottomNavigationItem (BottomBar)

--- ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
+++ ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
@@ -2,12 +2,13 @@ package com.example.composecatalog
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -19,23 +20,77 @@ fun MyScaffol() {
     val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
-        topBar = { MyTopAppBar {
-            coroutineScope.launch {
-                scaffoldState.snackbarHostState.showSnackbar("Has pulsado $it")
+        topBar = {
+            MyTopAppBar {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = "Has pulsado $it",
+                        duration = SnackbarDuration.Short,
+                        actionLabel = "Algo"
+                    )
+                }
             }
-        } },
+        },
+        scaffoldState = scaffoldState,
+        bottomBar = { MyBottomNavigation() }
 
     ) {
+    }
+}
 
+@Composable
+fun MyBottomNavigation() {
+
+    var index by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    BottomNavigation(
+        backgroundColor = Color(170, 0, 0),
+        contentColor = Color.White
+    ) {
+        BottomNavigationItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "boton"
+                )
+            },
+            label = { Text(text = "Home") }
+        )
+        BottomNavigationItem(
+            selected = index == 1,
+            onClick = { index = 1},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "boton"
+                )
+            },
+            label = { Text(text = "Home") }
+        )
+        BottomNavigationItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "boton"
+                )
+            },
+            label = { Text(text = "Home") }
+        )
     }
 
 }
 
 @Composable
-fun MyTopAppBar(onIconClick:(String) -> Unit) {
+fun MyTopAppBar(onIconClick: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Primera app") },
-        backgroundColor = Color(170,0,0),
+        backgroundColor = Color(170, 0, 0),
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
