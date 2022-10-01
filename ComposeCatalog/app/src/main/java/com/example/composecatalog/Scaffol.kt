1b16ce8c964286e164be0a5df490df1819f51e20
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-30 23:11:40 -0300

    add snackbar

--- ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
+++ ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
@@ -2,19 +2,29 @@ package com.example.composecatalog
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun MyScaffol() {
 
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
-        topBar = { MyTopAppBar() }
+        topBar = { MyTopAppBar {
+            coroutineScope.launch {
+                scaffoldState.snackbarHostState.showSnackbar("Has pulsado $it")
+            }
+        } },
+
     ) {
 
     }
@@ -22,14 +32,14 @@ fun MyScaffol() {
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onIconClick:(String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Primera app") },
-        backgroundColor = Color.Cyan,
+        backgroundColor = Color(170,0,0),
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { onIconClick("Atras") }) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
                     contentDescription = ""
@@ -37,15 +47,15 @@ fun MyTopAppBar() {
             }
         },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { onIconClick("Buscar") }) {
                 Icon(
                     imageVector = Icons.Filled.Search,
                     contentDescription = ""
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { onIconClick("Agregar") }) {
                 Icon(
-                    imageVector = Icons.Filled.Share,
+                    imageVector = Icons.Filled.Add,
                     contentDescription = ""
                 )
             }
