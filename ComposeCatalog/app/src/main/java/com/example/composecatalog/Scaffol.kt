7c22a44620585d8d825c4b5712749d932e0a2a9a
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-30 23:59:59 -0300

    add navigation drawer

--- ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
+++ ComposeCatalog/app/src/main/java/com/example/composecatalog/Scaffol.kt
@@ -1,14 +1,15 @@
 package com.example.composecatalog
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -21,20 +22,58 @@ fun MyScaffol() {
 
     Scaffold(
         topBar = {
-            MyTopAppBar {
-                coroutineScope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar(
-                        message = "Has pulsado $it",
-                        duration = SnackbarDuration.Short,
-                        actionLabel = "Algo"
-                    )
+            MyTopAppBar(
+                onClickIcon = {
+                    coroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            message = "Has pulsado $it",
+                            duration = SnackbarDuration.Short,
+                            actionLabel = "Algo"
+                        )
+                    }
+                }, onClickMenu = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.open()
+                    }
                 }
-            }
+            )
         },
         scaffoldState = scaffoldState,
-        bottomBar = { MyBottomNavigation() }
+        bottomBar = { MyBottomNavigation() },
+        floatingActionButton = { MyFAB() },
+
+        //ignora el bottomBar y lo posiciona donde le digas
+        //isFloatingActionButtonDocked = true,
+        //floatingActionButtonPosition = FabPosition.Center
+
+        drawerContent = { MyModelDrawer {
+            coroutineScope.launch {
+                scaffoldState.drawerState.close()
+            }
+        } },
+        drawerGesturesEnabled = false
 
     ) {
+    }
+}
+
+@Composable
+fun MyModelDrawer(onCloseDrawe: () -> Unit) {
+    Column(Modifier.padding(8.dp)) {
+        Text(text = "Algo1", modifier = Modifier.fillMaxWidth().clickable { onCloseDrawe() })
+        Text(text = "Algo1", modifier = Modifier.fillMaxWidth().clickable { onCloseDrawe() })
+        Text(text = "Algo1", modifier = Modifier.fillMaxWidth().clickable { onCloseDrawe() })
+    }
+}
+
+@Composable
+fun MyFAB() {
+    FloatingActionButton(
+        onClick = { /*TODO*/ },
+        backgroundColor = Color.Green,
+        contentColor = Color.Magenta
+    ) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
     }
 }
 
@@ -62,7 +101,7 @@ fun MyBottomNavigation() {
         )
         BottomNavigationItem(
             selected = index == 1,
-            onClick = { index = 1},
+            onClick = { index = 1 },
             icon = {
                 Icon(
                     imageVector = Icons.Default.Person,
@@ -87,28 +126,31 @@ fun MyBottomNavigation() {
 }
 
 @Composable
-fun MyTopAppBar(onIconClick: (String) -> Unit) {
+fun MyTopAppBar(
+    onClickIcon: (String) -> Unit,
+    onClickMenu: () -> Unit
+) {
     TopAppBar(
         title = { Text(text = "Primera app") },
         backgroundColor = Color(170, 0, 0),
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = { onIconClick("Atras") }) {
+            IconButton(onClick = { onClickMenu() }) {
                 Icon(
-                    imageVector = Icons.Filled.ArrowBack,
+                    imageVector = Icons.Filled.Menu,
                     contentDescription = ""
                 )
             }
         },
         actions = {
-            IconButton(onClick = { onIconClick("Buscar") }) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(
                     imageVector = Icons.Filled.Search,
                     contentDescription = ""
                 )
             }
-            IconButton(onClick = { onIconClick("Agregar") }) {
+            IconButton(onClick = { onClickIcon("Agregar") }) {
                 Icon(
                     imageVector = Icons.Filled.Add,
                     contentDescription = ""
